56cbbb46842b219caff753215519c6004a23a394
Podlevskiy Viktor <vitca64rus@gmail.com>
2021-05-10 20:25:10 +0300

    make full function 'encode'/it's make conver text to hex and add hex in RGB image

--- logic_steganography.py
+++ logic_steganography.py
@@ -6,23 +6,37 @@ img = Image.open("foto.jpeg")
 def text_to_ascii(text):
     text_hex = ''
     for i in text:
-        hex = bin(ord (i))[2:]
-        while len(hex) < 8:
-            hex = '0' + hex;
-        text_hex += hex
+        hex_ascii = bin(ord(i))[2:]
+        while len(hex_ascii) < 8:
+            hex_ascii = '0' + hex_ascii
+        text_hex += hex_ascii
     return text_hex
 
 
 def encode(img, text):
     size = img.size
-    width = size[0] - 1
-    height = size[1] - 1
+    width = size[0]
+    height = size[1]
+    i_text = 0
     for i in range(width):
         for j in range(height):
             pix = img.getpixel((i, j))
-            r = bin(pix[0])[2:-1] + '1'
-            g = bin(pix[1])[2:-1]
-            b = bin(pix[2])[2:-1]
-            img.putpixel((i, j),(r, g, b))
+            try:
+                r = int(bin(pix[0])[2:-1] + text[i_text])
+            except IndexError:
+                return img
+            i_text += 1
+            try:
+                g = int(bin(pix[1])[2:-1] + text[i_text])
+            except IndexError:
+                return img
+            i_text += 1
+            try:
+                b = int(bin(pix[2])[2:-1] + text[i_text])
+            except IndexError:
+                return img
+            i_text += 1
+            img.putpixel((i, j), (r, g, b))
 
 
+encode(img, text_to_ascii('text'))
